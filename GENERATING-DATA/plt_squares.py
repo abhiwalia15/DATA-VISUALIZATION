d2e5a83c889e5cceabc4392162e2b689c0f5c3bd
Mrinal <waliamrinal@gmail.com>
2018-04-26 15:37:50 +0530

    generating data and data sets and plotting graphs

---
+++ GENERATING-DATA/plt_squares.py
@@ -0,0 +1,9 @@
+#just provide matplot with a number and it will do the rest.
+
+import matplotlib.pyplot as plt
+#pyplot contains a number of functions that help generate charts and plots.
+
+#store the numbers in a list.
+squares = [1,4,9,16,25]
+plt.plot(squares)
+plt.show()
