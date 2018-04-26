03cb009c0cf0515b82666e2333b8bb2ec3e80f0d
Mrinal <waliamrinal@gmail.com>
2018-04-27 05:26:52 +0530

    add this chart figure

--- GENERATING-DATA/scatter_squares.py
+++ GENERATING-DATA/scatter_squares.py
@@ -2,14 +2,17 @@ import matplotlib.pyplot as plt
 
 #to plot a single point use the scatter function by passing the x,y values in the parameter
 #s parameter is used to set the size of the dots.
-x = [1,2,3,4,5]
-y = [1,4,9,16,25]
-plt.scatter(x, y, s=100)
+x_values = list(range(1,1001))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, s=10)
 
 #set the chart title and labels axes.
 plt.title("SQUARE NUMBERS",fontsize=24)
 plt.xlabel("VALUES",fontsize=14)
 plt.ylabel("SQUARES OF VALUES",fontsize=14)
+
+#set the range for each axis.Here axis()function takes 2 parameters min and max points of x&y-axis.
+plt.axis([0,1100,0,1100000])
 
 #set size of tick labels.
 plt.tick_params(axis='both',which='major',labelsize=14)
