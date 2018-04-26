49ec8caef231afc39c0e2a5615e37c082e127f79
Mrinal <waliamrinal@gmail.com>
2018-04-27 05:10:53 +0530

    add this image figure file

--- GENERATING-DATA/scatter_squares.py
+++ GENERATING-DATA/scatter_squares.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 
 #to plot a single point use the scatter function by passing the x,y values in the parameter
 #s parameter is used to set the size of the dots.
-plt.scatter(2, 4, s=200)
+x = [1,2,3,4,5]
+y = [1,4,9,16,25]
+plt.scatter(x, y, s=100)
 
 #set the chart title and labels axes.
 plt.title("SQUARE NUMBERS",fontsize=24)
