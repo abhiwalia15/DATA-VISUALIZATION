93889d4c71b1808f64616e16d21762dded61a7de
Mrinal <waliamrinal@gmail.com>
2018-04-28 16:27:25 +0530

    add this random walk file

--- GENERATING-DATA/random_walk.py
+++ GENERATING-DATA/random_walk.py
@@ -9,3 +9,29 @@ class RandomWalk():
 		#all walk starts at(0,0).
 		self.x_values = [0]
 		self.y_values = [0]
+		
+	def fill_walk(self):
+		'''calculate all the points in the walk'''
+		#keep taking step untill the walk reaches its desired length
+		
+		while len(self.x_values) < self.num_points:
+			#decide which direction to go and how far to go in that direction.
+			x_direction = choice([-1,1])
+			x_distance = choice([0,1,2,3,4])
+			x_step = x_distance*x_direction
+			
+			y_direction = choice([-1,1])
+			y_distance = choice([0,1,2,3,4])
+			y_step = y_distance*y_direction
+			
+			#reject moves that go nowhere
+			if x_step == 0 and y_step == 0:
+				continue
+				
+			#calculate the next x and y values
+			next_x = self.x_values[-1]+x_step
+			next_y = self.y_values[-1]+y_step
+			self.x_values = self.x_values.append(next_x)
+			self.y_values = self.y_values.append(next_y)
+			
+			
