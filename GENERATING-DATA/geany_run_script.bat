dab66a537c07fc56256b5bfebfee002aed06d992
Mrinal <waliamrinal@gmail.com>
2018-04-28 16:48:59 +0530

    add lil change in rw_visual file

--- GENERATING-DATA/geany_run_script.bat
+++
@@ -1,6 +0,0 @@
-python "rw_visual.py"
-
-pause
-del "%0"
-
-pause
--- GENERATING-DATA/rw_visual.py
+++ GENERATING-DATA/rw_visual.py
@@ -3,7 +3,13 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 #make a random walk and plot the points
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+#keep making a new walk as long as the program is active
+while True:
+	rw = RandomWalk()
+	rw.fill_walk()
+	plt.scatter(rw.x_values, rw.y_values, s=15)
+	plt.show()
+	
+	keep_running = input("MAKE ANOTHER WALK?(y/n)")
+	if keep_running == 'n':
+		break
