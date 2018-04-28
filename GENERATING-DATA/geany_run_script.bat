7eb5deae963e46ea783aaa5243cc373c7e03e221
Mrinal <waliamrinal@gmail.com>
2018-04-28 16:40:03 +0530

    add random walk visuals file and graph images

---
+++ GENERATING-DATA/geany_run_script.bat
@@ -0,0 +1,6 @@
+python "rw_visual.py"
+
+pause
+del "%0"
+
+pause
--- GENERATING-DATA/random_walk.py
+++ GENERATING-DATA/random_walk.py
@@ -16,11 +16,11 @@ class RandomWalk():
 		
 		while len(self.x_values) < self.num_points:
 			#decide which direction to go and how far to go in that direction.
-			x_direction = choice([-1,1])
+			x_direction = choice([1,-1])
 			x_distance = choice([0,1,2,3,4])
 			x_step = x_distance*x_direction
 			
-			y_direction = choice([-1,1])
+			y_direction = choice([1,-1])
 			y_distance = choice([0,1,2,3,4])
 			y_step = y_distance*y_direction
 			
@@ -31,7 +31,7 @@ class RandomWalk():
 			#calculate the next x and y values
 			next_x = self.x_values[-1]+x_step
 			next_y = self.y_values[-1]+y_step
-			self.x_values = self.x_values.append(next_x)
-			self.y_values = self.y_values.append(next_y)
+			self.x_values.append(next_x)
+			self.y_values.append(next_y)
 			
 			
