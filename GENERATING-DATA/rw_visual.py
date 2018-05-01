750c96ca25d7b0f63322b0a6ebb85a111870d019
Mrinal <waliamrinal@gmail.com>
2018-05-02 02:47:35 +0530

    add this graph

--- GENERATING-DATA/rw_visual.py
+++ GENERATING-DATA/rw_visual.py
@@ -5,10 +5,10 @@ from random_walk import RandomWalk
 #make a random walk and plot the points
 #keep making a new walk as long as the program is active
 while True:
-	rw = RandomWalk()
+	rw = RandomWalk(50000)
 	rw.fill_walk()
 	point_numbers = list(range(rw.num_points))
-	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
 	
 	#emphasize the first and the last walk
 	plt.scatter(0, 0, c='green', edgecolor='none', s=100)
