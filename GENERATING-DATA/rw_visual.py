63b842228bbb6b98ebaf355303cc07124d0e2543
Mrinal <waliamrinal@gmail.com>
2018-05-02 02:33:34 +0530

    add thiese changes

--- GENERATING-DATA/rw_visual.py
+++ GENERATING-DATA/rw_visual.py
@@ -9,6 +9,10 @@ while True:
 	rw.fill_walk()
 	point_numbers = list(range(rw.num_points))
 	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+	
+	#emphasize the first and the last walk
+	plt.scatter(0, 0, c='green', edgecolor='none', s=100)
+	plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=100)
 	plt.show()
 	
 	keep_running = input("MAKE ANOTHER WALK?(y/n)")
