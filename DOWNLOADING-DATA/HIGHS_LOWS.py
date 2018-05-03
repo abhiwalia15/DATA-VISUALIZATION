c8e558c3b720066f43a9457434b0aa85eab935d6
Mrinal <waliamrinal@gmail.com>
2018-05-03 23:52:32 +0530

    add this temp data

--- DOWNLOADING-DATA/HIGHS_LOWS.py
+++ DOWNLOADING-DATA/HIGHS_LOWS.py
@@ -1,5 +1,6 @@
 import csv
 
+#get high temperatures from the file
 #open the file as file object
 filename = "sitka_weather_07-2014.csv"
 
@@ -9,5 +10,12 @@ with open(filename) as f:
 	#next() function returns the next line in the file
 	header_row = next(reader)
 
+	highs = []
+	for row in reader:
+		highs.append(row[1])
+	print(highs)	
+
+	#we use enumerate() function to read the index as well as values in the list.
 	for index, column_header in enumerate(header_row):
 		print(index, column_header)
+		
