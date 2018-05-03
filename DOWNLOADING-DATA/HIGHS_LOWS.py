d93ae616cf02d2d28bb7309121f1494e37c89458
Mrinal <waliamrinal@gmail.com>
2018-05-04 01:06:27 +0530

    dates and high temps plot

--- DOWNLOADING-DATA/HIGHS_LOWS.py
+++ DOWNLOADING-DATA/HIGHS_LOWS.py
@@ -1,7 +1,8 @@
 import csv
+from datetime import datetime
 from matplotlib import pyplot as plt
 
-#get high temperatures from the file
+#get dates & high temperatures from the file
 #open the file as file object
 filename = "sitka_weather_07-2014.csv"
 
@@ -12,8 +13,11 @@ with open(filename) as f:
 	header_row = next(reader)
 	
 	#read the row one temperatures data.
-	highs = []
+	dates, highs = [], []
 	for row in reader:
+		current_date = datetime.strptime(row[0], "%Y-%m-%d")
+		dates.append(current_date)
+
 		#convert the extraceted data into int form from the string format by using int() function.
 		#we are converting it into int() to read the data for visualization of matplotlib.
 		high = int(row[1])
@@ -27,11 +31,13 @@ with open(filename) as f:
 #plot data
 '''set pixels using dpi'''
 fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 #format plot
 plt.title("DAILY HIGH TEMPERATURES, JULY 2014",fontsize=24)
 plt.xlabel('', fontsize=16)
+#this draws the date labels diagonally so that they dont overlap
+fig.autofmt_xdate()
 plt.ylabel("TEMPERATURE(F)",fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
