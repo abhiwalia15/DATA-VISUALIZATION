514deebe0a5b9a0bd3a3657d8248f45a4759c0cf
Mrinal <waliamrinal@gmail.com>
2018-05-03 23:35:20 +0530

    add this comment

--- DOWNLOADING-DATA/HIGHS_LOWS.py
+++ DOWNLOADING-DATA/HIGHS_LOWS.py
@@ -4,7 +4,9 @@ import csv
 filename = "sitka_weather_07-2014.csv"
 
 with open(filename) as f:
+	#reader processes the first line of comma-separated values in the file and stores each as an item in a list
 	reader = csv.reader(f)
+	#next() function returns the next line in the file
 	header_row = next(reader)
 	print(header_row)
 	
