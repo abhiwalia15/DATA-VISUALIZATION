f387c37d9188370ea2f6311dda6e335e3f3d6079
Mrinal <waliamrinal@gmail.com>
2018-05-05 16:31:17 +0530

    s

--- DOWNLOADING-DATA/geany_run_script.bat
+++
@@ -1,6 +0,0 @@
-python "population_data.py"
-
-pause
-del "%0"
-
-pause
--- DOWNLOADING-DATA/population_data.py
+++ DOWNLOADING-DATA/population_data.py
@@ -8,5 +8,5 @@ with open(filename) as f:
 for pop_dict in pop_data:
 	if pop_dict['Year']=='2010':
 		country_name = pop_dict['Country Name']
-		population = pop_dict['Value']
-		print(country_name+':'+population)
+		population = int(pop_dict['Value'])
+		print(country_name+':'+str(population))
