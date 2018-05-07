5c0b0088967ede480707e983eb08377d62bd6f5b
Mrinal <waliamrinal@gmail.com>
2018-05-08 01:37:13 +0530

    world population map

--- DOWNLOADING-DATA/world_population.py
+++ DOWNLOADING-DATA/world_population.py
@@ -20,7 +20,24 @@ for pop_dict in pop_data:
 		if code:
 			cc_populations[code] = population
 		
+#group the countries into 3 groups
+#less than 10million , 10million to 1 billion and above 1 billion
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+	if pop<10000000:
+		cc_pops_1[cc] = pop
+	elif pop<1000000000:
+		cc_pops_2[cc] = pop
+	else:
+		cc_pops_3[cc] = pop	
+		
+#see how many countries are in each level
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+	
 wm=pygal.maps.world.World()
+
 wm.title='WORLD POPULATION IN 2010, BY COUNTRY'
-wm.add('2010', cc_populations)		
+wm.add('0-10m', cc_pops_1)		
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
 wm.render_to_file('world_population_map.svg')
