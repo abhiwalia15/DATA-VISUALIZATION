dab10ef53bd2aeafe4f0b51b5edb7d9fa2ab1d67
Mrinal <waliamrinal@gmail.com>
2018-06-17 23:52:26 +0530

    add info

--- Working With APIs(Application Programming Interface)/python_repos.py
+++ Working With APIs(Application Programming Interface)/python_repos.py
@@ -15,10 +15,16 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 
 #create 2 empty lists for storing names and no. of stars.
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
 	names.append(repo_dict['name'])
-	stars.append(repo_dict['stargazers_count'])
+	
+	plot_dict = {
+			'value': repo_dict['stargazers_count'],
+			'label': repo_dict['description'],
+			'xlink': repo_dict['html_url'],
+			}
+	plot_dicts.append(plot_dict)
 	
 #make visualization
 my_style = LS('#333366', base_style = LCS)
@@ -40,10 +46,10 @@ my_config.width = 1000
 chart = pygal.Bar(my_config, style = my_style)
 chart.title = 'MOST -STARRED PYTHON PROJECTS ON GITHUB'
 chart.x_label = names
-chart.add(' ', stars)
+chart.add(' ', plot_dicts)
 chart.render_to_file('python_repos.svg')
  
-
+'''
 print("repositories returned: ",len(repo_dicts))
 print("SELECTED INFORMATION ABOUT EACH REPOSITORY:")
 #loop through each of repository.
@@ -55,7 +61,7 @@ for repo_dict in repo_dicts:
 	print("CREATED:", repo_dict['created_at'])
 	print("UPDATED:", repo_dict['updated_at'])
 	print("DESCRIPTION:", repo_dict['description'])
-
+'''
 
 
 
